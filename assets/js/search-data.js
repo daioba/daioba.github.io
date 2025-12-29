acdaa327fb0b5f375619373fe2de693e1acb206f
daioba <49937258+daioba@users.noreply.github.com>
2025-12-29 18:24:33 +0000

    Deploying to gh-pages from @ daioba/daioba.github.io@f51610cbb40355148d0d1c54e3ae6e30f47a9df0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -386,10 +386,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-a-new-web-page-has-been-created",
+      },{id: "news-a-new-web-page-has-been-created",
           title: 'A new Web Page has been created.',
           description: "",
           section: "News",},{
